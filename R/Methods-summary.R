2aac0a5a743bcb7c97d109e46a76b97a2a10532b
jonkatz2 <jonkatz2@gmail.com>
2018-01-12 17:10:07 -0500

    clean start

--- R/Methods-summary.R
+++
@@ -1,6 +0,0 @@
-# Summary methods for objects
-# Modified: 2013 JUNE 14
-
-setMethod('summary', 'TemplateList', function(object) {show(object)})
-setMethod('summary', 'templateScores', function(object) {show(object)})
-setMethod('summary', 'detectionList', function(object) {show(object)})
